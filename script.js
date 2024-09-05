6efc4eeee628b9dafb0c50fdfad69fc5837e7cd2
uksharma <mysrms2021@gmail.com>
2024-09-05 20:47:56 +0530

    fixed icon issue

--- script.js
+++ script.js
@@ -19,14 +19,26 @@ async function checkWeather(city) {
         document.querySelector(".temp").textContent = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").textContent = data.main.humidity + " %";
         document.querySelector(".wind").textContent = data.wind.speed + " Kmph";
+        const weatherIcon = document.querySelector(".weather-icon");
         
-        const weatherCondition = data.weather[0].main;
-        weatherIcon.src = `./images/${weatherCondition.toLowerCase()}.png`;
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "./images/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "./images/clear.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "./images/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "./images/mist.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "./images/rain.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "./images/snow.png";
+        }
+        
 
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
 
-        // Clear the input field
         searchBox.value = "";
     } catch (error) {
         document.querySelector(".error").style.display = "block";
@@ -34,7 +46,6 @@ async function checkWeather(city) {
     }
 }
 
-// Add event listeners for button click and 'Enter' key
 searchBtn.addEventListener("click", () => {
     if (searchBox.value !== "") {
         checkWeather(searchBox.value);
